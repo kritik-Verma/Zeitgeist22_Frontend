363ce0e60d2b60fe95ab80e643ba308e696e8524
kritik <kritikverma2002@gmail.com>
2021-12-13 22:29:23 +0530

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sass/Nav.scss";
+
+function Nav() {
+  return <div className="nav"></div>;
+}
+
+export default Nav;
---
+++ src/page/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/Nav";
+import "./sass/Home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+      <Nav />
+    </div>
+  );
+}
+
+export default Home;
